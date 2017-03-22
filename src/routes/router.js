ec6e1f5877e877dd86cae59d8dacb596da7a23d9
lujing <jing.lu@chejianer.com>
2017-03-22 10:59:51 +0800

    history

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,14 @@
 /**
  * Created by Administrator on 2017/3/21.
  */
-import {Router, Route, IndexRoute, Redirect, hashHistory} from 'react-router';
+import {Router, Route, IndexRoute, Redirect, hashHistory , browserHistory} from 'react-router';
 import React, {Component, PropTypes} from 'react';
 import Index from '../components';
 
 import 'normalize.css';
 import '../stylesheets/index.scss';
 
+const history = process.env.NODE_ENV == 'production'?browserHistory:hashHistory
 
 const Roots = (props)=>(
     <div>
@@ -16,7 +17,7 @@ const Roots = (props)=>(
 )
 
 const rootRoute = (
-    <Router history={hashHistory}>
+    <Router history={history}>
         <Route path="/" component={Roots}>
             <IndexRoute component={Index}/>
             <Redirect from='*' to='/'/>
